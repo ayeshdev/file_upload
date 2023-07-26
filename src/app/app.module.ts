9a1c361eb12ae2c942d1aa2461c5d482f03287a9
Ayesh Niroshan <ayeshniroshan88@gmail.com>
2023-07-26 22:13:53 +0530

    file_uploading version 1.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,41 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { provideStorage,getStorage } from '@angular/fire/storage';
+import { AngularFireModule } from '@angular/fire/compat';
+import {AngularFireStorageModule} from'@angular/fire/compat/storage';
+import { UploadDetailsComponent } from './components/upload-details/upload-details.component';
+import { UploadFormComponent } from './components/upload-form/upload-form.component';
+import { UploadListComponent } from './components/upload-list/upload-list.component';
+import { AngularFireDatabase, AngularFireDatabaseModule, AngularFireList } from '@angular/fire/compat/database';
+import { AngularFireStorage } from '@angular/fire/compat/storage';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UploadDetailsComponent,
+    UploadFormComponent,
+    UploadListComponent
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserModule,
+    AngularFireDatabaseModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(environment.firebase),
+AngularFireModule,
+AngularFireStorageModule,
+
   ],
-  providers: [],
+  providers: [
+    AngularFireDatabase,
+    AngularFireStorage,
+    AngularFireStorageModule],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'crud-a9e7a',
+    appId: '1:704538682788:web:60e1f42640b8f74ea24d31',
+    storageBucket: 'crud-a9e7a.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyAEYRCdcnQcnuwNqmX3u4BD-gvcYo5ALeg',
+    authDomain: 'crud-a9e7a.firebaseapp.com',
+    messagingSenderId: '704538682788',
+  },
+};
